1d0d44bb6e1129dae9ac6d89e551da2a76b7036a
sublime247 <kushimobashir2000@gmail.com>
2024-09-23 16:58:41 +0100

    changes

--- contracts/InvoisChain.sol
+++ contracts/InvoisChain.sol
@@ -11,15 +11,17 @@ contract InvoiceChain is ReentrancyGuard {
         address buyer;
         address seller;
         string[] goods;
-        string date;
+        uint date;
         uint amount;
         bool isPaid;
         address tokenAddress;
         uint amountPaidInEther;
+        uint invoiceId;
     }
 
     mapping(uint256 => Invoice) public invoices;
-    uint public invoiceId;
+    Invoice[] listOfInvoice;
+    uint256 _invoiceCount;
 
     constructor() {}
 
@@ -29,7 +31,11 @@ contract InvoiceChain is ReentrancyGuard {
         }
     }
 
-    function validateBalanceAndApproval(address _tokenAddress, address from, uint _amt) private view {
+    function validateBalanceAndApproval(
+        address _tokenAddress,
+        address from,
+        uint _amt
+    ) private view {
         IERC20 token = IERC20(_tokenAddress);
         uint bal = token.balanceOf(from);
         uint allowance = token.allowance(from, address(this));
@@ -53,37 +59,59 @@ contract InvoiceChain is ReentrancyGuard {
     function createInvoice(
         address _buyerAddress,
         string[] memory _goods,
-        uint _amt,
+        uint _totalAmt,
         address _tokenAddress
     ) external {
         zeroAddress(msg.sender);
         zeroAddress(_tokenAddress);
+        zeroAddress(_buyerAddress);
 
-        uint invId = invoiceId + 1;
-        Invoice storage _invoice = invoices[invId];
-
-        _invoice.buyer = _buyerAddress;
-        _invoice.goods = _goods;
-        _invoice.amount = _amt;
-        _invoice.seller = msg.sender;
-        _invoice.tokenAddress = _tokenAddress;
-
-        invoiceId += 1;
-
-        emit Events.InvoiceCreatedSuccessfully(_buyerAddress, _amt, invId);
+        uint256 _invoiceId = _invoiceCount++;
+        Invoice memory _invoice = Invoice({
+            buyer: _buyerAddress,
+            seller: msg.sender,
+            goods: _goods,
+            date: block.timestamp,
+            amount: _totalAmt,
+            isPaid: false,
+            tokenAddress: _tokenAddress,
+            invoiceId: _invoiceId,
+            amountPaidInEther: 0
+        });
+        invoices[_invoiceId] = _invoice;
+        listOfInvoice.push(_invoice);
+        emit Events.InvoiceCreatedSuccessfully(
+            _buyerAddress,
+            _invoice.amount,
+            _invoice.invoiceId
+        );
     }
 
     function payInvoice(uint _invoiceId) external nonReentrant {
         zeroAddress(msg.sender);
         validateInvoiceID(_invoiceId);
-
+        require(_invoiceId > 0, "Invalid Id");
         Invoice storage _invoice = invoices[_invoiceId];
-        validateBalanceAndApproval(_invoice.tokenAddress, msg.sender, _invoice.amount);
-        
-        IERC20(_invoice.tokenAddress).transferFrom(msg.sender, _invoice.seller, _invoice.amount);
+        require(_invoice.buyer == msg.sender, "Invalid Id");
+        require(_invoice.invoiceId == _invoiceId, "Invalid Id");
+        validateBalanceAndApproval(
+            _invoice.tokenAddress,
+            msg.sender,
+            _invoice.amount
+        );
+
+        IERC20(_invoice.tokenAddress).transferFrom(
+            msg.sender,
+            _invoice.seller,
+            _invoice.amount
+        );
         _invoice.isPaid = true;
 
-        emit Events.PaymentSuccessful(_invoice.seller, _invoice.amount, _invoiceId);
+        emit Events.PaymentSuccessful(
+            _invoice.seller,
+            _invoice.amount,
+            _invoiceId
+        );
     }
 
     function payWithEther(uint _invoiceId) external payable nonReentrant {
@@ -91,22 +119,38 @@ contract InvoiceChain is ReentrancyGuard {
         validateInvoiceID(_invoiceId);
 
         Invoice storage _invoice = invoices[_invoiceId];
+        require(_invoice.buyer == msg.sender, "Invalid Id");
+        require(_invoice.invoiceId == _invoiceId, "Invalid Id");
         require(msg.value == _invoice.amount, "Incorrect Ether amount sent");
         require(!_invoice.isPaid, "Invoice already paid");
 
         _invoice.amountPaidInEther = msg.value;
         _invoice.isPaid = true;
 
-        emit Events.PaymentSuccessful(_invoice.seller, _invoice.amount, _invoiceId);
+        emit Events.PaymentSuccessful(
+            _invoice.seller,
+            _invoice.amount,
+            _invoiceId
+        );
     }
 
     function sendEtherToSeller(uint _invoiceId) external nonReentrant {
+        zeroAddress(msg.sender);
+        validateInvoiceID(_invoiceId);
         Invoice storage _invoice = invoices[_invoiceId];
-        require(msg.sender == _invoice.seller, "Only the seller can withdraw funds");
+        require(_invoice.seller == msg.sender, "Invalid Id");
+        require(_invoice.invoiceId == _invoiceId, "Invalid Id");
+        require(
+            msg.sender == _invoice.seller,
+            "Only the seller can withdraw funds"
+        );
         require(_invoice.isPaid, "Invoice not paid");
 
         uint amount = _invoice.amountPaidInEther;
-        require(address(this).balance >= amount, "Insufficient contract balance");
+        require(
+            address(this).balance >= amount,
+            "Insufficient contract balance"
+        );
 
         _invoice.amountPaidInEther = 0;
         payable(_invoice.seller).transfer(amount);
@@ -114,7 +158,13 @@ contract InvoiceChain is ReentrancyGuard {
         emit Events.PaymentSuccessful(_invoice.seller, amount, _invoiceId);
     }
 
-    function generateInvoice(uint _invoiceId) external view returns(Invoice memory) {
+    function generateInvoice(
+        uint _invoiceId
+    ) external view returns (Invoice memory) {
         return invoices[_invoiceId];
+    }
+
+    function generateAllInvoices() external view returns (Invoice[] memory) {
+        return listOfInvoice;
     }
 }
